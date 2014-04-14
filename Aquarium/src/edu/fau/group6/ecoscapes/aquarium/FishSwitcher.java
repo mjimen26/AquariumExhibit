f1abbad9401af2c7b54ea8737f5f0bcd56638d39
Marvin <fadestaynight@gmail.com>
2014-04-13 20:53:44 -0400

    Added DialogBox to Living Reef Tank

---
+++ Aquarium/src/edu/fau/group6/ecoscapes/aquarium/FishSwitcher.java
@@ -0,0 +1,24 @@
+package edu.fau.group6.ecoscapes.aquarium;
+
+public class FishSwitcher {
+	
+	public static String[] livingReef = {
+		"-The Blue Tang appears on Finding Nemo as Dory! \n\n-The Blue Tang can easily be found at pet stores if you want to raise one as your pet!",
+		"-The Doctorfish Tang's \"scalpels\" are mainly used as defensive machanism when fighting against other fishes",
+		"-The Red-Spotted Hawkfish is also called Granny Hawkfish \n\n-The Red-Spotted Hawkfish is very aggressive and it should not be kept around larger Hawkfishes",
+		"-The porkfish got its name because of the grunty sounds it makes when it rubs its teeth together",
+		"-Flamefishes spend most of their time in dark, shady places during the day, and come out after dark",
+		"-It is recomended to keep one male Pygmy Angelfish at most since they may fight to death",
+		"-Fairy Basslets are often seen swimming upside-down under ledges and overhangs",
+		"-Nobody has an idea how the Slippery Dick got its name, but some believe it is because of its body shape and texture",
+		"-Most Yellowhead Wrasse are observed to bully Yellowhead Jawfishes by coming inside their burrows and harrsing them",
+		"-Turbo Snails are not harvested from July-September due to the hot climate in Mexico. However, Margarita Snails serve as a substitue during those months",
+		"-Pincushion Urchins sold at pet stores often come in very vibrant colors of purple, turquoise, and pink",
+		"-Red Serpent Stars are very sensitve of the water chemistry. They cannot tolerate sudden changes in oxygen levels, salinity, and pH levels",
+		"-The Serranus family are species of fish that includes groupers. Their family are often identified having very interesting patterns.",
+		"-Among the Serranus family, the Saddle Bass has the most plain color and has almost no pattern in its body.",
+		"-The Harlequin Bass got its name from the harlequins, or performers often clothed in white with unique patterns of several colors",
+		"-The Bluehead Wrasse is recommended to be the last one added in an aquarium since it would eat smaller fish and harrass new fishes"
+	};
+
+}
--- Aquarium/src/edu/fau/group6/ecoscapes/aquarium/livingreef/LivingReef.java
+++ Aquarium/src/edu/fau/group6/ecoscapes/aquarium/livingreef/LivingReef.java
@@ -2,10 +2,15 @@ package edu.fau.group6.ecoscapes.aquarium.livingreef;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import edu.fau.group6.ecoscapes.aquarium.FishSwitcher;
 import edu.fau.group6.ecoscapes.aquarium.R;
 import edu.fau.group6.ecoscapes.aquarium.adapters.LivingViewAdapter;
 
@@ -15,12 +20,14 @@ public class LivingReef extends FragmentActivity implements
 	private ViewPager viewPager;
 	private LivingViewAdapter mAdapter;
 	private ActionBar actionBar;
+	Context context = this;
+
 	// Tab titles
 	private String[] tabs = { "Blue Tang", "Doctorfish", "Hawkfish",
 			"Porkfish", "Flamefish", "Angelfish", "Fairy Basslet",
-			"Slippery Dick", "Yellow Wrasse", "Turbo Snail",
-			"Pincushion", "Serpent Star", "Orange Back", 
-			"Saddle Bass", "Harlequin", "Blue Wrasse"};
+			"Slippery Dick", "Yellow Wrasse", "Turbo Snail", "Pincushion",
+			"Serpent Star", "Orange Back", "Saddle Bass", "Harlequin",
+			"Blue Wrasse" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +48,23 @@ public class LivingReef extends FragmentActivity implements
 			actionBar.addTab(actionBar.newTab().setText(tab_name)
 					.setTabListener(this));
 		}
-		
+
 		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-		 
-		    @Override
-		    public void onPageSelected(int position) {
-		        // on changing the page
-		        // make respected tab selected
-		        actionBar.setSelectedNavigationItem(position);
-		    }
-		 
-		    @Override
-		    public void onPageScrolled(int arg0, float arg1, int arg2) {
-		    }
-		 
-		    @Override
-		    public void onPageScrollStateChanged(int arg0) {
-		    }
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
 		});
 	}
 
@@ -76,7 +83,65 @@ public class LivingReef extends FragmentActivity implements
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 	}
 
+	public void onClick(View v) {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				context);
+
+		// Set the title
+		alertDialogBuilder.setTitle("Did You Know?");
+
+		alertDialogBuilder.setMessage(fishSwitcher(v)).setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		alertDialog.show();
+
+	}
+
+	public String fishSwitcher(View v) {
+		View view = v;
+
+		switch (view.getId()) {
+
+		case R.id.bluetang_image:
+			return FishSwitcher.livingReef[0];
+		case R.id.doctorfish_image:
+			return FishSwitcher.livingReef[1];
+		case R.id.hawkfish_image:
+			return FishSwitcher.livingReef[2];
+		case R.id.porkfish_image:
+			return FishSwitcher.livingReef[3];
+		case R.id.flamefish_image:
+			return FishSwitcher.livingReef[4];
+		case R.id.angelfish_image:
+			return FishSwitcher.livingReef[5];
+		case R.id.fairybass_image:
+			return FishSwitcher.livingReef[6];
+		case R.id.slipperydick_image:
+			return FishSwitcher.livingReef[7];
+		case R.id.yellowhead_image:
+			return FishSwitcher.livingReef[8];
+		case R.id.turbosnail_image:
+			return FishSwitcher.livingReef[9];
+		case R.id.pincushion_image:
+			return FishSwitcher.livingReef[10];
+		case R.id.redstar_image:
+			return FishSwitcher.livingReef[11];
+		case R.id.orangeback_image:
+			return FishSwitcher.livingReef[12];
+		case R.id.saddlebass_image:
+			return FishSwitcher.livingReef[13];
+		case R.id.harlequin_image:
+			return FishSwitcher.livingReef[14];
+		case R.id.bluehead_image:
+			return FishSwitcher.livingReef[15];
+		}
+
+		return "No such Fish";
+	}
+
 }
-
-
-
